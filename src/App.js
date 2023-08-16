2b46c0d7774466918e760c34e3c24ef9e148e43e
EduardoGraffe <Eduardoagf1@gmail.com>
2023-08-15 21:34:09 -0500

    c

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      <h2 className="text-3xl font-bold underline">
         Testing to see concurrent tasks being performed in code.
-      </h1>
+      </h2>
     </div>
   );
 }
