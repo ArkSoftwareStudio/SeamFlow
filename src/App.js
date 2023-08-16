fdb720d279d1952d4d632ccd8ea3d5119edcc4ae
consuegra.luisg@gmail.com <consuegra.luisg@gmail.com>
2023-08-15 22:39:38 -0400

    Everything Merged

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
       <h2 className="text-3xl font-bold underline">
         Testing to see concurrent tasks being performed in code.
       </h2>
+      <h1 className="text-3xl font-bold underline">
+        Testing to see concurrent tasks being performed in code.
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Testing to see if changes to same files work!
+      </h1>
     </div>
   );
 }
